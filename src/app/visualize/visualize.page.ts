5d6b04a8112227af28b040d965317cb0a3f46543
Giosuè <sulipano.gs@gmail.com>
2019-09-25 12:39:14 +0200

    Re-create visualization page

--- src/app/visualize/visualize.page.ts
+++ src/app/visualize/visualize.page.ts
@@ -1,29 +1,15 @@
-import { Component } from '@angular/core';
-import { FileAnalyzerService } from '../file-analyzer.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-visualize',
   templateUrl: './visualize.page.html',
   styleUrls: ['./visualize.page.scss'],
 })
-export class VisualizePage {
-  fileDebug: string = "Nothing to analyze."
+export class VisualizePage implements OnInit {
 
-  constructor(public fileAnalyzer: FileAnalyzerService) {
-    //empty
+  constructor() { }
+
+  ngOnInit() {
   }
 
-  analyzeFile($event) {
-    let fileUploaded = $event.target.files[0];
-    console.log('FILE UPLOAD EVENT: ', $event);
-    console.log('FILE UPLOADED: ', fileUploaded);
-    console.log ('FILE TYPE: ', fileUploaded instanceof File);
-    this.fileAnalyzer.data2JSON(fileUploaded).then((data) => {
-      this.fileDebug = JSON.stringify(data);
-    }, (reason) => {
-      this.fileDebug = reason;
-    }).catch((exception) => {
-      this.fileDebug = "Error while calling the function 'data2JSON'!";
-    });
-  }
 }
