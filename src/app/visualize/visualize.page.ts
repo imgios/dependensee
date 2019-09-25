7b9701dabebff2b0db415f76c857ffc436a654f1
Giosuè <sulipano.gs@gmail.com>
2019-09-25 14:57:32 +0200

    Add file upload function

--- src/app/visualize/visualize.page.ts
+++ src/app/visualize/visualize.page.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visualize.page.scss'],
 })
 export class VisualizePage implements OnInit {
+  fileLogs: String = "";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  analyzeFile($event) {
+    let fileUploaded = $event.target.files[0];
+    console.log(fileUploaded);
+    this.fileLogs += "[!] File upload event fired\n";
+    let isFile = (fileUploaded instanceof File) ? 'Yes' : 'No';
+    this.fileLogs += "[!] Is a file? " + isFile + "\n";
+    this.fileLogs += "[!] File uploaded: " + fileUploaded + "\n";
+    this.fileLogs += "[!] File extension: " + fileUploaded.name.split('.').pop() + "\n";
   }
 
 }
